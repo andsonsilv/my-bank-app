fa758bbbcf63896af9f105ed3457ce6bf4e7ad66
Andson <andsonsilva@alu.ufc.br>
2021-10-18 01:10:44 -0300

    Refatorando o código

--- lib/main.dart
+++ lib/main.dart
@@ -6,26 +6,48 @@ void main() {
       appBar: AppBar(
         title: const Text('Transferências'),
       ),
+      body: ListaTransferencias(),
       floatingActionButton:
           FloatingActionButton(child: const Icon(Icons.add), onPressed: () {}),
-      body: Column(
-        children: const [
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('100.0'),
-              subtitle: Text('1000'),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('200.0'),
-              subtitle: Text('4000'),
-            ),
-          ),
-        ],
-      ),
     ),
   ));
+}
+
+class ListaTransferencias extends StatelessWidget {
+  ListaTransferencias();
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ItemTransferencia(Transferencia(100.0, 1234)),
+        ItemTransferencia(Transferencia(300.0, 5434)),
+        ItemTransferencia(Transferencia(123.5, 1222)),
+        ItemTransferencia(Transferencia(10928.54, 1222)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: const Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
 }
